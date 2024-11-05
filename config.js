0dc1f644b5b67089e1618cf8b9770b10294157c5
UnknownLua <129235434+devycreates@users.noreply.github.com>
2024-11-04 21:22:10 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  OWNER_IDS: [""], // Bot owner ID's
-  SUPPORT_SERVER: "", // Your bot support server
+  OWNER_IDS: ["1089202294454747186"], // Bot owner ID's
+  SUPPORT_SERVER: "https://discord.gg/KJqRu3K6VV", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
     DEFAULT_PREFIX: "!", // Default prefix for the bot
